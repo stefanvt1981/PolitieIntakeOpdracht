9ccf1a2df14a7eb491c1f644b61a2e193f5051ae
Leen Tuit <leentuit@gmail.com>
2020-12-04 18:01:12 +0100

    Added Current Time Controller

---
+++ PolitieIntakeOpdracht/Controllers/CurrentTimeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolitieIntakeOpdracht.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrentTimeController : ControllerBase
+    {
+        private readonly ILogger<CurrentTimeController> _logger;
+
+        public CurrentTimeController(ILogger<CurrentTimeController> logger) => _logger = logger;
+
+        [HttpGet("{cityName}/{countryCode?}")]
+        public string Get(string cityName, string countryCode = null)
+        {
+            return $"{cityName}, {(string.IsNullOrEmpty(countryCode) ? "Unknown" : countryCode)}";
+        }
+    }
+}
