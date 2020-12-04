adab55f517bde7d3fcddab02089bb507de9b631f
Leen Tuit <leentuit@gmail.com>
2020-12-04 18:04:12 +0100

    Added Suspect class

---
+++ PolitieIntakeOpdracht/Suspect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PolitieIntakeOpdracht
+{
+    public class Suspect
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public int? Age { get; set;  }
+
+        [Required]
+        public string CriminalAct { get; set; }
+
+        [Required]
+        public string ArrestLocation { get; set; }
+
+        [Required]
+        public DateTime? Date { get; set; }
+    }
+}
